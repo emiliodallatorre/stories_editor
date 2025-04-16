4164b5f2d60e5ff5e48ba292b984c214ae3c8367
matteogennaersaf <m.genna@ersaf.it>
2025-04-16 16:24:59 +0200

    problema di rendering corretto

--- lib/src/presentation/text_editor_view/widgets/background_color_selector.dart
+++ lib/src/presentation/text_editor_view/widgets/background_color_selector.dart
@@ -14,48 +14,41 @@ class BackgroundColorSelector extends StatelessWidget {
         
         return Container(
           height: 80,
+          width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
             color: Colors.black38,
             borderRadius: BorderRadius.circular(10),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Color selection grid
-              Expanded(
-                child: GridView.builder(
-                  padding: const EdgeInsets.all(8),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 7, 
-                    crossAxisSpacing: 8,
-                    mainAxisSpacing: 8,
-                  ),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: colors.length,
-                  itemBuilder: (context, index) {
-                    final color = colors[index];
-                    return GestureDetector(
-                      onTap: () {
-                        // Imposta il colore di sfondo usando il metodo specifico per testo obbligatorio
-                        editorNotifier.setMandatoryTextBackgroundColor(color);
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: color,
-                          border: Border.all(
-                            color: editorNotifier.backGroundColor == color
-                                ? Colors.white
-                                : Colors.transparent,
-                            width: 2,
-                          ),
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                      ),
-                    );
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+            itemCount: colors.length,
+            itemBuilder: (context, index) {
+              final color = colors[index];
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6),
+                child: GestureDetector(
+                  onTap: () {
+                    // Imposta il colore di sfondo usando il metodo specifico per testo obbligatorio
+                    editorNotifier.setMandatoryTextBackgroundColor(color);
                   },
+                  child: Container(
+                    width: 48,
+                    height: 48,
+                    decoration: BoxDecoration(
+                      color: color,
+                      border: Border.all(
+                        color: editorNotifier.backGroundColor == color
+                            ? Colors.white
+                            : Colors.transparent,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              );
+            },
           ),
         );
       },
